887e6904dc4017148136363b6cf71c46733497b9
rubenvl <ruben.vander.linden@outlook.be>
2020-11-06 10:25:55 +0100

    Added Helper project and  ConfigHelper class

---
+++ Maw.Helpers/ConfigHelper.cs
@@ -0,0 +1,11 @@
+﻿using System.Configuration;
+
+namespace Maw.Helpers
+{
+    public static class ConfigHelper
+    {
+        public static string GetConnectionString(string name) =>
+            ConfigurationManager.ConnectionStrings[name].ConnectionString;
+        public static string GetConfigValue(string name) => ConfigurationManager.AppSettings.Get(name);
+    }
+}
